0eb80c16f2b8ed41d5233d4a88e672f54b66e411
otavioaam <102493452+otavioaam@users.noreply.github.com>
2023-09-27 18:30:38 -0300

    Add files via upload
    Foi adicionado o campo de inserir a senha do admin para acessar o menu.

--- cartorio_otavioaam.c
+++ cartorio_otavioaam.c
@@ -112,54 +112,72 @@ int deletar()
 }
 
 int main()
-{
+	{
 	int opcao=0; //Definindo variáveis
 	int laco=1;
 	
-	for(laco=1;laco=1;)
+	char senhadigitada[]="a";
+	int comparacao;
+	
+	printf("### Cartório da EBAC ## \n\n");
+	printf("Login de ADM! \n\n Digite a sua senha: ");
+	scanf("%s",senhadigitada);
+	
+	comparacao = strcmp(senhadigitada, "admin");
+	
+	if(comparacao == 0)
 	{
-	
-		system("cls"); //responsavel por limpar a tela
-	
-		setlocale(LC_ALL, "Portuguese"); //Definindo a linguagem
-	
-		printf("\n\n\t ### Cartorio da EBAC ###\n\n"); //Início do MENU
-		printf("\t Escolha a opção desejada do menu:\n\n");
-		printf("\t 1- Registrar nomes\n");
-		printf("\t 2- Consultar Nomes\n");
-		printf("\t 3- Deletar Nomes\n\n"); 
-		printf("\t 4- Sair do sistema \n\n");
-		printf("Opção: ");//Final do MENU
-	
-		scanf("%d", &opcao); //Armazenando a escolha do usuário
-	
-		system("cls");
-		
-		switch(opcao) //inicio da seleção do menu
+		system ("cls");
+		for(laco=1;laco=1;)
 		{
-			case 1:
-			registro();	//chamada de funções (registro)
-			break;
+	
+			system("cls"); //responsavel por limpar a tela
+	
+			setlocale(LC_ALL, "Portuguese"); //Definindo a linguagem
+	
+			printf("\n\n\t ### Cartorio da EBAC ###\n\n"); //Início do MENU
+			printf("\t Escolha a opção desejada do menu:\n\n");
+			printf("\t 1- Registrar nomes\n");
+			printf("\t 2- Consultar Nomes\n");
+			printf("\t 3- Deletar Nomes\n\n"); 
+			printf("\t 4- Sair do sistema \n\n");
+			printf("Opção: ");//Final do MENU
+	
+			scanf("%d", &opcao); //Armazenando a escolha do usuário
+	
+			system("cls");
+		
+			switch(opcao) //inicio da seleção do menu
+			{
+				case 1:
+				registro();	//chamada de funções (registro)
+				break;
+				
+				case 2:
+				consulta(); //chamada de funções (consulta)
+				break;
+				
+				case 3:
+				deletar(); //chamada de funções (deletar)
+				break;	
+				
+				case 4:
+				printf("Obrigado por utilizar o sistema :)\n");
+				return 0;
+				break;
+				
+				default: //caso o usuario escreva outra opção além de 1 - 3, o sistema da essa mensagem abaixo
+				printf("\n\n Não existe essa opção, por favor volte ao menu!\n");
+				system("pause");
+				break;
+			} //Fim da selecao
 			
-			case 2:
-			consulta(); //chamada de funções (consulta)
-			break;
-			
-			case 3:
-			deletar(); //chamada de funções (deletar)
-			break;	
-			
-			case 4:
-			printf("Obrigado por utilizar o sistema :)\n");
-			return 0;
-			break;
-			
-			default: //caso o usuario escreva outra opção além de 1 - 3, o sistema da essa mensagem abaixo
-			printf("\n\n Não existe essa opção, por favor volte ao menu!\n");
-			system("pause");
-			break;
 		}
 	}
+	
+	else
+		printf("Senha incorreta");
+	
 }
 
 
